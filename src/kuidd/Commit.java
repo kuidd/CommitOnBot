03936c10ea063d9272822e90b7c9f587af1a0254
Dmitry Usynin <d.usynin@ftc.ru>
2017-03-30 23:44:49 +0700

    implement Commit

--- src/kuidd/Commit.java
+++ src/kuidd/Commit.java
@@ -1,10 +1,24 @@
 package kuidd;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Commit implements ICommit {
-	public Commit(String response) {
+	private JSONObject json;
+
+	public Commit(String json) {
+		try {
+			this.json = new JSONObject(json);
+		} catch (JSONException e) {
+		}
 	}
 
 	@Override
 	public void notify(IChat chat) {
+		try {
+			chat.print(json.getJSONObject("head_commit").getJSONObject("author").get("name") + " committed on "
+					+ json.getJSONObject("repository").get("name"));
+		} catch (JSONException e) {
+		}
 	}
 }
