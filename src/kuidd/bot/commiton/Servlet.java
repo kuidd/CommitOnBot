45eb45050f61e22f3ab19fc7d68930a79beb61bc
Dmitry Usynin <d.usynin@ftc.ru>
2017-04-20 09:55:24 +0700

    #22 add 200 response

--- src/kuidd/bot/commiton/Servlet.java
+++ src/kuidd/bot/commiton/Servlet.java
@@ -22,6 +22,10 @@ public class Servlet extends AbstractHandler {
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+		baseRequest.setHandled(true);
+
 		if (!target.equals("/message"))
 			return;
 
